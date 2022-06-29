6d81abcc8b9205008e8da590c22513f926efa931
Edu-Lu <moralesluis17106494@gmail.com>
2022-06-29 14:10:20 -0500

    Initial commit

---
+++ app/src/main/java/com/luismorales17106494/a022_funcionesparmetrosyretorno/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.luismorales17106494.a022_funcionesparmetrosyretorno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+22 - Funciones: parámetros y retorno de datos tipo arreglo
+
+Hemos visto el objetivo de plantear funciones en un programa y que las mismas pueden
+recibir datos por medio de parámetros y retornar un dato.
+
+Los parámetros de una función pueden ser de tipo Int, Char, Float etc. como hemos visto
+en conceptos anteriores pero también pueden ser de tipo arreglo como veremos en este concepto.
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
